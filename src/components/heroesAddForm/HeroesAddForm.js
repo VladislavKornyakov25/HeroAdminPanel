373b863cb351d49dc66492865ca3259c61023c92
Vlad Kornyakov <time2begin666@gmail.com>
2024-04-18 17:42:34 +0300

    Finish

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,53 +1,31 @@
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
-
-import {useHttp} from '../../hooks/http.hook';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
-import { heroCreated } from '../heroesList/heroesSlice';
+
 import { selectAll } from '../heroesFilters/filterSlice';
 import store from '../../store';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
-const HeroesAddForm = () => {
-    // Состояния для контроля формы
+const HeroesAddForm = () => {   
     const [heroName, setHeroName] = useState('');
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
+    const [createHero, {isLoading}] = useCreateHeroMutation();
+
     const {filtersLoadingStatus} = useSelector(state => state.filters);
-    const filters = selectAll(store.getState());
-    const dispatch = useDispatch();
-    const {request} = useHttp();
+    const filters = selectAll(store.getState());  
 
     const onSubmitHandler = (e) => {
-        e.preventDefault();
-        // Можно сделать и одинаковые названия состояний,
-        // хотел показать вам чуть нагляднее
-        // Генерация id через библиотеку
+        e.preventDefault();       
         const newHero = {
             id: uuidv4(),
             name: heroName,
             description: heroDescr,
             element: heroElement
         }
-
-        // Отправляем данные на сервер в формате JSON
-        // ТОЛЬКО если запрос успешен - отправляем персонажа в store
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
-
-        // Очищаем форму после отправки
+        createHero(newHero).unwrap();       
         setHeroName('');
         setHeroDescr('');
         setHeroElement('');
@@ -59,14 +37,11 @@ const HeroesAddForm = () => {
         } else if (status === "error") {
             return <option>Ошибка загрузки</option>
         }
-        
-        // Если фильтры есть, то рендерим их
+  
         if (filters && filters.length > 0 ) {
-            return filters.map(({name, label}) => {
-                // Один из фильтров нам тут не нужен
+            return filters.map(({name, label}) => {               
                 // eslint-disable-next-line
                 if (name === 'all')  return;
-
                 return <option key={name} value={name}>{label}</option>
             })
         }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import heroes from '../components/heroesList/heroesSlice';
 import filters from '../components/heroesFilters/filterSlice';
+import { apiSlice } from '../api/apiSlice';
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -12,8 +12,11 @@ const stringMiddleware = () => (next) => (action) => {
 }
 
 const store = configureStore({
-    reducer: {heroes, filters},
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    reducer: {         
+        filters, 
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production',
     
 });
